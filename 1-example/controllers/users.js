d176f15c506f9fddfa19c3699432a314f36ba485
Gavin Garza <gaving@live.com>
2024-01-08 18:10:30 +0900

    connection tables for membership and user_notes added added to db

--- 1-example/controllers/users.js
+++ 1-example/controllers/users.js
@@ -1,14 +1,24 @@
 const router = require("express").Router()
 const { tokenExtractor, isAdmin } = require("../utils/middleware")
 
-const { User, Note } = require("../models")
+const { User, Note, Team } = require("../models")
+const UserNotes = require("../models/user_notes")
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ["userId"] },
-    },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
   })
   res.json(users)
 })
@@ -23,7 +33,34 @@ router.post("/", async (req, res) => {
 })
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id)
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: [""] },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Note,
+        as: "marked_notes",
+        attributes: { exclude: ["userId"] },
+        through: {
+          attributes: [],
+        },
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  })
   if (user) {
     res.json(user)
   } else {
